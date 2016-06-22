2b3740ba5d29bb18bea4c21e5754933d2e7a9331
Mudrak <mudrakpatel96@gmail.com>
2016-06-22 13:48:14 -0500

    Content in Milos and Hydra

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,7 @@
 // setup your IIFE (Immediately Invoked Function Expression)
-// Comp125-S2016-MidTerm-300878960
+/**
+ * Comp125-S2016-MidTerm-300878960
+ */
 /**
  * @author: Mudrak Patel
  * @website name: The Travel Report
@@ -25,13 +27,17 @@ var Alonissos=document.getElementById("Alonissos");
 Alonissos.innerHTML="Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream."+"<br>"+" The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes."+"<br>"+" Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy."+"<br>";
 //Spetses content injecting process
 var Spetses=document.getElementById("Spetses");
-Spetses.innerHTML="This bohemian paradise is more Côte d'Azur than Grecian island."+"<br>"+" Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques."+"<br>"+" Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight.";
+Spetses.innerHTML="This bohemian paradise is more Côte d'Azur than Grecian island."+"<br>"+" Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques."+"<br>"+" Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight."+"<br>";
 //Amorgos content injecting process
 var Amorgos=document.getElementById("Amorgos");
-Amorgos.innerHTML="A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners."+"<br>"+" Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways.";
+Amorgos.innerHTML="A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners."+"<br>"+" Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways."+"<br>";
 //Syros content injecting process
 var Syros=document.getElementById("Syros");
-Syros.innerHTML="A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners."+"<br>"+" Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways.";
+Syros.innerHTML="A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners."+"<br>"+" Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways."+"<br>";
+var Milos=document.getElementById("Milos");
+Milos.innerHTML="This volcanic island, with catacombs and ancient iron mines to explore, is dramatically rugged, and famous for its stunning rock formations. Often likened to a moonscape, it's also known for its hot springs, the ancient Venus De Milo statue that was found here, and for its diversity of incredible beaches. Known as "+"the island of colours"+ "it's home to around 80 beaches — some only accessible by boat — ranging from stunningly white, to striking black, and even unusual red and grey. However, each beach has the same Evian-clear turquoise waters, and is surrounded by a rugged mountain landscape."+"<br>";
+var Hydra=document.getElementById("Hydra");
+Hydra.innerHTML="The first thing you'll notice on this beautiful island is the lack of cars — and buses, motorcycles, or other wheeled vehicles, as well as high rises. The winding little streets full of beautiful 18th-century mansions, churches, cathedrals, museums and art galleries are covered in cobblestones, and mostly trafficked by donkeys and humans."+"<br>"+" Back in the day, the island attracted celebrities like Leonard Cohen and Sophia Loren, but was somehow forgotten over the years. That means that travellers to Hydra can have the chic yet almost rural island paradise more or less to themselves."+"<br>";
 
 })();
 
