3ca861d8006835453e872fc25eeddbfe195934e8
Mudrak <mudrakpatel96@gmail.com>
2016-06-22 13:15:01 -0500

    First commit

---
+++ Scripts/app.js
@@ -0,0 +1,23 @@
+// setup your IIFE (Immediately Invoked Function Expression)
+// Comp125-S2016-MidTerm-300878960
+/**
+ * @author: Mudrak Patel
+ * @website name: The Travel Report
+ * @Comp125-S2016-MidTerm-300878960
+ */
+(function () {
+"use strict";
+/**
+ * @ part A
+ */
+var IntroParagraph=document.getElementById("IntroParagraph");
+IntroParagraph.innerHTML="Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals."+
+"<br>"+
+"Greece has 1,400 islands, though only 230 of them are inhabited."+"<br>" 
+
+"And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too."+"<br>";
+
+
+
+})();
+
