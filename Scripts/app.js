66ee070891c7cbc08a08e2afd1cf2a25a97aeb84
Mudrak <mudrakpatel96@gmail.com>
2016-06-22 13:36:32 -0500

    Content in Amorgos paragraph

--- Scripts/app.js
+++ Scripts/app.js
@@ -26,6 +26,9 @@ Alonissos.innerHTML="Off the mainland, this island, whose surrounding waters are
 //Spetses content injecting process
 var Spetses=document.getElementById("Spetses");
 Spetses.innerHTML="This bohemian paradise is more Côte d'Azur than Grecian island."+"<br>"+" Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques."+"<br>"+" Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight.";
+//Amorgos content injecting process
+var Amorgos=document.getElementById("Amorgos");
+Amorgos.innerHTML="A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners."+"<br>"+" Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways.";
 
 })();
 
